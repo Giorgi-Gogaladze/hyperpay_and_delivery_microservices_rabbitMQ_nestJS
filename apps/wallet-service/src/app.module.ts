0121842d5b7e61951203f7213fa214ba0d318392
GiorgiGogaladze <gogaladzegio55@gmail.com>
2026-07-12 23:12:38 +0400

    feat: implementing env validations in service modules

--- apps/wallet-service/src/app.module.ts
+++ apps/wallet-service/src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { evnValidationSchema } from '../config/env.validation';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: evnValidationSchema,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
