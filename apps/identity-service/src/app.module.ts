24a5025e8c8f24e537cade80d249e02db5d20c86
GiorgiGogaladze <gogaladzegio55@gmail.com>
2026-07-13 22:59:54 +0400

    feat (identity-service): connecting prisma to the database

--- apps/identity-service/src/app.module.ts
+++ apps/identity-service/src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 
 @Module({
@@ -12,6 +13,7 @@ import { UserModule } from './user/user.module';
       envFilePath: '.env',
       validationSchema: envValidationSchema,
     }),
+    PrismaModule,
     AuthModule,
     UserModule,
   ],
