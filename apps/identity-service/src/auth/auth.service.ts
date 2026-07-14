d8e6856655e1febad7fd3c6a6b912d1ce3086403
GiorgiGogaladze <gogaladzegio55@gmail.com>
2026-07-14 16:50:27 +0400

    feat(identity-service): (auth) creating registration, token creating and refreshtoken hashing(with argon and crypto) funcitons in service

--- apps/identity-service/src/auth/auth.service.ts
+++ apps/identity-service/src/auth/auth.service.ts
@@ -1,4 +1,89 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+import { RegisterDto } from './dtos/register.dto';
+import { Role, User } from '../generated/prisma/client';
+import * as argon2 from 'argon2'
+import * as crypto from 'crypto'
+import { JwtService } from '@nestjs/jwt';
+
+type SafeUser = Omit<User, 'password' | 'refreshToken'>; 
+
+interface IAuthTokens {
+    accessToken: string;
+    refreshToken: string;
+}
+
+interface IAuthResponse {
+    user: SafeUser;
+    tokens: IAuthTokens;
+}
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly Prisma: PrismaService,
+        private readonly jwtService: JwtService
+    ){}
+
+    async register(dto: RegisterDto): Promise<IAuthResponse>{
+        const existingUser = await this.Prisma.user.findUnique({
+            where: {email: dto.email},
+        });
+
+        if(existingUser){
+            throw new ConflictException(`User with email: ${dto.email} already exist!`)
+        };
+
+        const hashedPassword: string = await argon2.hash(dto.password);
+
+        const user: User = await this.Prisma.user.create({
+            data: {
+                firstName: dto.firstName,
+                lastName: dto.lastName,
+                email: dto.email,
+                password: hashedPassword,
+                role: Role.USER,
+            },
+        });
+
+        const tokens: IAuthTokens = await this.generateTokens(user.id, user.email, user.role);
+        await this.updateRefreshTokenHash(user.id, tokens.refreshToken);
+
+        const {password, refreshToken, ...safeUser} = user;
+        return {user: safeUser, tokens};
+    }
+
+
+
+    private async generateTokens(userId: string, email: string, role: string): Promise<IAuthTokens>{
+        const payload = {sub: userId, email, role};
+
+        const [accessToken, refreshToken] = await Promise.all([
+            this.jwtService.signAsync(payload, {
+                secret: process.env.JWT_ACCESS_SECRET,
+                expiresIn: '15m',
+            }),
+            this.jwtService.signAsync(payload, {
+                secret: process.env.JWT_REFRESH_SECRET,
+                expiresIn: '7d',
+            }),
+        ]);
+        return {accessToken, refreshToken};
+    }
+
+
+    private async updateRefreshTokenHash(
+        userId: string,
+        refreshToken: string
+    ): Promise<void>{
+        const hash: string = crypto
+            .createHash('sha256')
+            .update(refreshToken)
+            .digest('hex');
+        
+        await this.Prisma.user.update({
+            where: { id: userId},
+            data: { refreshToken: hash}
+        })
+    }
+}
--- apps/identity-service/src/auth/dtos/register.dto.ts
+++ apps/identity-service/src/auth/dtos/register.dto.ts
@@ -1,5 +1,4 @@
-import { Role } from "../../generated/prisma/enums";
-import { IsEmail, IsEnum, IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from 'class-validator'
+import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator'
 
 export class RegisterDto {
     @IsString()
@@ -20,8 +19,4 @@ export class RegisterDto {
     @MinLength(8)
     @MaxLength(72)
     password: string;
-
-    @IsOptional()
-    @IsEnum(Role)
-    role?: Role
 }
