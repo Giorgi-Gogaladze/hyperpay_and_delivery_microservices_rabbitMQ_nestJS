21b0a06231842ae9936c95ffd33228a0e1428e0a
GiorgiGogaladze <gogaladzegio55@gmail.com>
2026-07-21 14:22:57 +0400

    feat(wallet): implement wallet creation logic and user event handling

--- apps/wallet-service/src/wallet/wallet.controller.ts
+++ apps/wallet-service/src/wallet/wallet.controller.ts
@@ -1,7 +1,28 @@
 import { Controller } from '@nestjs/common';
 import { WalletService } from './wallet.service';
+import { Ctx, EventPattern, Payload, RmqContext } from '@nestjs/microservices';
 
 @Controller('wallet')
 export class WalletController {
   constructor(private readonly walletService: WalletService) {}
+
+  @EventPattern('user.created')
+  async handleUserCreatd(
+    @Payload() data: {userId: string},
+    @Ctx() context: RmqContext //ამით ვეუბნებით, რომ რადნა მეინში noAck: არის ფოლსი, ეუბნება: RabbitMQ, მესიჯი რიგიდან არ წაშალო, სანამ wallet-service ხელით არ დაგიდასტურებს (Ack), რომ ის წარმატებით დამუშავდა
+  ){
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+
+    try {
+      await this.walletService.createWalletForUser(data.userId);
+      channel.ack(originalMsg); //ვეუბნები RabbitMQ-ს "ყველაფერმა კარგად ჩაიარა, წაშალე მესიჯი რიგიდან!
+    } catch (error: any) {
+
+      console.error('wallet creation failed:', error);
+
+      channel.nack(originalMsg, false, false)//თუ შეცდომა მოხდა, ეუბნები RabbitMQ-ს "ვერ დავამუშავე, ისევ რიგში ჩააბრუნე. პარამეტრები: (მესიჯი, multiple, requeue)
+    }
+
+  }
 }
--- apps/wallet-service/src/wallet/wallet.service.ts
+++ apps/wallet-service/src/wallet/wallet.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
-export class WalletService {}
+export class WalletService {
+    private readonly logger = new Logger(WalletService.name);
+
+    constructor( private readonly prisma: PrismaService ){};
+
+    async createWalletForUser(userId: string): Promise<void>{
+       const existing = await this.prisma.wallet.findUnique({
+        where: {userId},
+       });
+
+       if(existing){
+           this.logger.warn('Wallet for this user already exist')
+           return;
+       }
+
+       await this.prisma.wallet.create({
+        data: {userId},
+       })
+
+       this.logger.log(`Wallet created for user ${userId}`);
+    }
+}
+          
