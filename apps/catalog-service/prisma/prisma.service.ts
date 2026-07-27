625ce5453b050fd7541bdeccea499c0741c21105
GiorgiGogaladze <gogaladzegio55@gmail.com>
2026-07-27 15:31:42 +0400

    feat(catalog-service): creating prisma service and module fiels with onmoduleinit and destroy to connect to the database

---
+++ apps/catalog-service/prisma/prisma.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+import { PrismaClient } from "../src/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleDestroy, OnModuleInit{
+    constructor(){
+        const adapter = new PrismaPg({
+            connectionString: process.env.CATALOG_DATABASE_URL,
+        });
+
+        super({adapter})
+    }
+
+    async onModuleInit() {
+        await this.$connect()
+    }
+
+    async onModuleDestroy() {
+        await this.$disconnect()
+    }
+}
