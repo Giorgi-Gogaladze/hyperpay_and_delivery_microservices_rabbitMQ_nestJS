54167bee3da42537e49c9e3dec44c4defb6ceb60
GiorgiGogaladze <gogaladzegio55@gmail.com>
2026-07-27 19:13:06 +0400

    refactor(identity-service): refactoring code structure, putting services in modules dir

--- apps/catalog-service/src/app.module.ts
+++ apps/catalog-service/src/app.module.ts
@@ -2,6 +2,9 @@ import { envValidationSchema } from '@app/common/config/env.validation';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from '../prisma/prisma.module';
+import { TaxonomyModule } from './modules/taxonomy/taxonomy.module';
+import { CatalogModule } from './modules/catalog/catalog.module';
+import { ReviewsModule } from './modules/reviews/reviews.module';
 
 @Module({
   imports: [
@@ -11,6 +14,9 @@ import { PrismaModule } from '../prisma/prisma.module';
       validationSchema: envValidationSchema,
     }),
     PrismaModule,
+    TaxonomyModule,
+    CatalogModule,
+    ReviewsModule,
   ],
   controllers: [],
   providers: [],
