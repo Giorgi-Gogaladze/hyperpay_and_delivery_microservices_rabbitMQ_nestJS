3563c78c646eeb5caf9a6d48c6534dbecfa502d9
GiorgiGogaladze <gogaladzegio55@gmail.com>
2026-07-28 17:36:44 +0400

    feat(categories): implement CRUD operations for categories controller

--- apps/catalog-service/src/modules/taxonomy/categories/categories.controller.ts
+++ apps/catalog-service/src/modules/taxonomy/categories/categories.controller.ts
@@ -1,8 +1,53 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseUUIDPipe, Patch, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { Category } from '../../../generated/prisma/client';
+import { FileInterceptor } from '@nestjs/platform-express'
+import { CreateCategoryDto } from './dtos/create-category.dto';
+import { UpdateCategoryDto } from './dtos/update-category.dto';
 
 
 @Controller('categories')
 export class CategoriesController {
-  
+  constructor( private readonly categoriesService: CategoriesService ){}
+
+  @Get()
+  async getAllCategories(): Promise<Category[]>{
+    return await this.categoriesService.getAllCategories();
+  }
+
+  @Get(':id')
+  async getCategoryById(
+    @Param('id', ParseUUIDPipe) id: string,
+  ): Promise<Category> {
+    return this.categoriesService.getCategoryById(id);
+  }
+
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  async createCategory(
+    @Body() dto: CreateCategoryDto,
+    @UploadedFile() file?: Express.Multer.File
+  ): Promise<Category>{
+    return this.categoriesService.createCategory(dto, file);
+  }
+
+
+  @Patch(':id')
+  @UseInterceptors(FileInterceptor('file'))
+  async updateCategory(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() dto: UpdateCategoryDto,
+    @UploadedFile() file?: Express.Multer.File,
+  ): Promise<Category> {
+    return this.categoriesService.updateCategoy(id, dto, file);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.OK)
+  async deleteCategory(
+    @Param('id', ParseUUIDPipe) id: string,
+  ): Promise<{ message: string }> {
+    return this.categoriesService.deleteCategory(id);
+  }
 
 }
