74a36999ee9d4f9d74d59de4a9629e6ce137d4de
GiorgiGogaladze <gogaladzegio55@gmail.com>
2026-07-29 12:15:04 +0400

    feat(brands): implement createBrand method for brand creation

--- apps/catalog-service/src/modules/taxonomy/brands/brands.service.ts
+++ apps/catalog-service/src/modules/taxonomy/brands/brands.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../../../../prisma/prisma.service';
+import { CloudinaryService } from '../../../shared/cloudinary/cloudinary.service';
+import { CreateBrandDto } from './dtos.ts/create-brand.dto';
+import { Brand } from '../../../generated/prisma/client';
+import slugify from 'slugify'
 
 @Injectable()
-export class BrandsService {}
+export class BrandsService {
+    constructor(
+        private readonly prisma: PrismaService,
+        private readonly cloudinaryService: CloudinaryService,
+    ){}
+
+    async createBrand(
+        dto: CreateBrandDto,
+        file?: Express.Multer.File
+    ): Promise<Brand>{
+        const existing = await this.prisma.brand.findFirst({
+            where: {name: dto.name},
+        });
+
+        if(!existing){
+            throw new ConflictException(`Brand with name: ${dto.name} already exist`)
+        };
+
+        const brandSlug = slugify(dto.name, {
+            strict: true,
+            lower: true,
+            replacement: '-'
+        });
+
+        let logoImg = null;
+        let logoPublicId = null;
+        if(file){
+            const uploadRes = await this.cloudinaryService.uploadImage(file, 'brands');
+            logoImg = uploadRes.secure_url;
+            logoPublicId = uploadRes.public_id;
+        };
+
+        return await this.prisma.brand.create({
+            data: {
+                name: dto.name,
+                slug: brandSlug,
+                description: dto.description,
+                isActive: dto.isActive ?? true,
+                logoImg, 
+                logoPublicId
+            }
+        })
+
+        
+    }
+}
