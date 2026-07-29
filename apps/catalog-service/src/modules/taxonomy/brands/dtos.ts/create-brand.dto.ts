eda54a499a6314cd234f4652447989d9534a3c08
GiorgiGogaladze <gogaladzegio55@gmail.com>
2026-07-29 12:05:27 +0400

    feat(brands): add CreateBrandDto and UpdateBrandDto for brand management

---
+++ apps/catalog-service/src/modules/taxonomy/brands/dtos.ts/create-brand.dto.ts
@@ -0,0 +1,21 @@
+import { Transform } from "class-transformer";
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, MinLength } from "class-validator";
+
+export class CreateBrandDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(2)
+    name: string;
+
+    @IsOptional()
+    @IsString()
+    description: string;
+
+    @IsBoolean()
+    @Transform(({value}) => {
+        if(value === 'true' || value === true) return true;
+        if(value === 'false' || value === false) return false;
+        return true;
+    })
+    isActive?: boolean = true;
+}
