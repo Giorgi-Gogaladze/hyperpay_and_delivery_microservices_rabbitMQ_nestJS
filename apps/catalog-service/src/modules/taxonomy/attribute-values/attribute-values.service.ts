fbfad8e248c9391a45b5f4815827dda07906c3cd
GiorgiGogaladze <gogaladzegio55@gmail.com>
2026-07-30 15:41:52 +0400

    feat(attribute-values): implement createAttributeValues method with validation for attribute existence

--- apps/catalog-service/src/modules/taxonomy/attribute-values/attribute-values.service.ts
+++ apps/catalog-service/src/modules/taxonomy/attribute-values/attribute-values.service.ts
@@ -1,9 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../../../../prisma/prisma.service';
+import { CreateAttributeValueDto } from './dtos/createAttributeValue.dto';
 
 @Injectable()
 export class AttributeValuesService {
-    constructor(private readonly pirsma: PrismaService){}
+    constructor(private readonly prisma: PrismaService){}
 
-    
+    async createAttributeValues(
+        dto: CreateAttributeValueDto
+    ): Promise<{ message: string; count: number }>{
+        
+        const attribute = await this.prisma.attribute.findUnique({
+        where: { id: dto.attributeId },
+        });
+
+        if (!attribute) {
+        throw new NotFoundException(
+            `Attribute with ID "${dto.attributeId}" not found`,
+            );
+        };
+
+        const recordToCreate = dto.values.map((val) => ({
+            attributeId: dto.attributeId,
+            value: val
+        }));
+
+        const res = await this.prisma.attributeValue.createMany({
+            data: recordToCreate,
+            skipDuplicates: true,
+        });
+
+        return {
+            message: `${res.count} attribute value(s) created successfully`,
+            count: res.count
+        }
+    }
 }
