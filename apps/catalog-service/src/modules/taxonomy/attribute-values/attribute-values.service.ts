8da3800e980c2d5e2fa52cabadea769a9a51fe54
GiorgiGogaladze <gogaladzegio55@gmail.com>
2026-07-30 16:22:23 +0400

    feat(attribute-values): add updateAttributeValue with its dto and deleteAttribute function

--- apps/catalog-service/src/modules/taxonomy/attribute-values/attribute-values.service.ts
+++ apps/catalog-service/src/modules/taxonomy/attribute-values/attribute-values.service.ts
@@ -1,6 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../../../../prisma/prisma.service';
 import { CreateAttributeValueDto } from './dtos/createAttributeValue.dto';
+import { AttributeValue } from '../../../generated/prisma/client';
+import { UpdateAttributeValueDto } from './dtos/updateAttributeValue.dto';
 
 @Injectable()
 export class AttributeValuesService {
@@ -34,5 +36,69 @@ export class AttributeValuesService {
             message: `${res.count} attribute value(s) created successfully`,
             count: res.count
         }
+    }
+
+
+    async updateAttributeValue(
+        id: string,
+        dto: UpdateAttributeValueDto
+    ): Promise<AttributeValue>{
+        const existing = await this.prisma.attributeValue.findUnique({
+            where: { id },
+        });
+
+        if (!existing) {
+            throw new NotFoundException(`Attribute value with ID "${id}" not found`);
+        }
+
+        if(dto.value !== existing.value){
+            const duplicate = await this.prisma.attributeValue.findFirst({
+            where: { 
+                attributeId: existing.attributeId,
+                value: dto.value,
+                NOT: {id}   
+            },
+        });
+
+            if (duplicate) {
+                throw new ConflictException(
+                    `Value "${dto.value}" already exists for this attribute`,
+                );
+            };
+        };
+
+        return this.prisma.attributeValue.update({
+            where: { id },
+            data: { value: dto.value },
+        });
+    }
+
+
+
+    async deleteAttributeValue(
+        attId: string, 
+        attValueid: string
+    ): Promise<{message: string}>{
+       const res = await this.prisma.attributeValue.deleteMany({
+            where: {
+                id: attValueid,
+                attributeId: attId,
+            },
+       });
+       
+       if(res.count === 0){
+            throw new NotFoundException(
+                `Attribute value with ID "${attValueid}" not found for attribute "${attId}"`,
+            );
+       }
+
+       await this.prisma.attributeValue.delete({
+        where: {id: attValueid}
+       });
+
+       return {
+        message: 'AttributeValue removed successfully'
+       }
+
     }
 }
