107ef9cdfca20c3ce61ea151a7af7c66fa4f047b
GiorgiGogaladze <gogaladzegio55@gmail.com>
2026-07-31 15:56:54 +0400

    feat(review-module): enhance ReviewsController with create, update, and delete review methods

--- apps/catalog-service/src/modules/review/reviews/reviews.controller.ts
+++ apps/catalog-service/src/modules/review/reviews/reviews.controller.ts
@@ -1,7 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, HttpCode, HttpStatus, Param, ParseUUIDPipe, Patch, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '@app/common';
+import { User } from '@app/common/decorators/get-current-user.decorator';
+import { Role } from '@app/common/decorators/roles.decorator';
+import { Review } from '../../../generated/prisma/client';
+import { CreateReviewDto } from './dtos/create-review.dto';
+import { UpdateReviweDto } from './dtos/update-review.dto';
 import { ReviewsService } from './reviews.service';
 
 @Controller('reviews')
+@UseGuards(JwtAuthGuard)
 export class ReviewsController {
   constructor(private readonly reviewsService: ReviewsService) {}
+
+  @Post(':productId')
+  async createReview(
+    @Body() dto: CreateReviewDto,
+    @Param('productId', ParseUUIDPipe) productId: string,
+    @User('id') userId: string,
+  ): Promise<Review> {
+    return this.reviewsService.createReview(dto, productId, userId);
+  }
+
+  @Patch(':id')
+  async updateReview(
+    @Param('id', ParseUUIDPipe) reviewId: string,
+    @Body() dto: UpdateReviweDto,
+  ): Promise<Review> {
+    return this.reviewsService.updateReview(reviewId, dto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.OK)
+  async deleteReview(
+    @Param('id', ParseUUIDPipe) reviewId: string,
+    @User('id') userId: string,
+    @User('role') role?: string,
+  ): Promise<{ message: string }> {
+    return this.reviewsService.deleteReview(reviewId, userId, role === Role.ADMIN);
+  }
 }
