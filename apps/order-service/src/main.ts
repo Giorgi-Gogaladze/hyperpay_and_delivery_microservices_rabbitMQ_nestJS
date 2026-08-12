6e0531e24f430a9ac3ef7d7c97a055b2f9f55928
GiorgiGogaladze <gogaladzegio55@gmail.com>
2026-08-12 12:01:54 +0400

    feat(order-service): integrate RabbitMQ microservice setup for getting emit messages

--- apps/order-service/src/main.ts
+++ apps/order-service/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { createValidationPipe } from '@app/common/pipes/validation-pipe.factory';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,10 +10,26 @@ async function bootstrap() {
   app.useGlobalPipes(createValidationPipe());
   app.enableCors();
 
-  const configService = app.get(ConfigService)
+  const configService = app.get(ConfigService);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [configService.getOrThrow<string>('RABBITMQ_URL') || 'amqp://localhost:5672'],
+      queue: 'order_queue',
+      queueOptions: {
+        durable: true,
+      },
+      noAck: false,
+    }
+  });
+
+  await app.startAllMicroservices();
+
   const port = configService.getOrThrow<number>('ORDER_PORT')
 
   await app.listen(port);
-  console.log('Identity service has started')
+  console.log('Order service has started');
+  console.log('Order Rmq has started');
 }
 bootstrap();
